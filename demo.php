e630d5332fafaa75e2f8194731b83bfecca55a54
misumoo <cewhetzel@gmail.com>
2016-03-20 16:25:00 -0400

    Update
    Added Model class
    Added subclasses for address types

--- demo.php
+++ demo.php
@@ -29,15 +29,25 @@ $address_residence->street_address_1 = '555 Fake Street';
 $address_residence->city_name = 'Townsville';
 $address_residence->subdivision_name = 'State';
 $address_residence->country_name = 'United States of America';
-$address_residence->address_type_id = 3;
 echo $address_residence;
 echo '<pre>' . var_export($address_residence, TRUE) . '</pre>';
 
 echo "<h2>Testing __construct with an array</h2>";
-$address2 = new Address(array(
+$address_business = new AddressBusiness(array(
   'street_address_1' => '123 Phony Avenue',
   'city_name' => 'VillageLand',
   'subdivision_name' => 'Region',
   'country_name' => 'Canada'
 ));
-echo $address2;
+echo "<pre>" . var_export($address_business, TRUE) . "</pre>";
+
+echo "<h2>Instantiating AddressPark</h2>";
+$address_park = new AddressPark(array(
+  'street_address_1' => '789 Missing Circle',
+  'street_address_2' => 'Suite 0',
+  'city_name' => 'Hamlet',
+  'subdivision_name' => 'Territory',
+  'country_name' => 'Australia',
+));
+echo $address_park;
+echo "<pre>" . var_export($address_park, TRUE) . "</pre>";
